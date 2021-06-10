fe436341e4aad4a48a5b86ca849afe5e1ff991e9
sumon <sumoniceiu1415@gmail.com>
2021-06-10 09:31:23 +0600

    Ado.net example

--- Adonetexample1/Adonetexample1/Program.cs
+++ Adonetexample1/Adonetexample1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace Adonetexample1
 {
@@ -6,7 +7,85 @@ namespace Adonetexample1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            using SqlConnection conncection = new SqlConnection();
+
+            conncection.ConnectionString = "Server=DESKTOP-PAVS32T\\SQLEXPRESS;Database=Aspdotnetb5;User Id=aspdotnetb5;Password=aspdotnetb5";
+
+            if(conncection.State==System.Data.ConnectionState.Closed)
+            {
+                conncection.Open();
+
+            }
+
+            var sql = "delete from Customer where PostalCode=234";
+            using SqlCommand command2 = new SqlCommand(sql, conncection);
+            command2.ExecuteNonQuery();
+
+            Console.WriteLine("delete successfully");
+            Console.WriteLine();
+
+            var sql2 = "select * from Customer";
+            ReadOperation(sql2, conncection);
+
+
+
+            var sql3="insert into Customer(Customername,ContactName,PostalCode,Country)values('rahim','Rangpur',55,'japan')";
+            WriteOperation(sql3, conncection);
+            Console.WriteLine();
+
+            Console.WriteLine("after insserting the table");
+            Console.WriteLine();
+
+            var sql4 = "select * from Customer";
+            ReadOperation(sql4, conncection);
+
+
+
+            /*
+             using SqlCommand command = new SqlCommand("Select * from Customer",conncection);
+
+             using SqlDataReader sqlr = command.ExecuteReader();
+
+             while(sqlr.Read())
+             {
+                 Console.WriteLine(sqlr["CustomerName"] + "  " +sqlr["ContactName"]+" "+sqlr["PostalCode"]+" "+sqlr["Country"]);
+
+
+             }
+
+             conncection.Close();
+            */
+
+        }
+
+        static  void ReadOperation(string sql,SqlConnection connection)
+        {
+            
+            if(connection.State==System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+
+            }
+            
+            using SqlCommand command = new SqlCommand(sql, connection);
+            using SqlDataReader sqlr = command.ExecuteReader();
+
+            while(sqlr.Read())
+            {
+                Console.WriteLine(sqlr["CustomerName"] + "  " + sqlr["ContactName"] + " " + sqlr["PostalCode"] + " " + sqlr["Country"]);
+            }
+
+
+        }
+        static void WriteOperation(string sql,SqlConnection connection)
+        {
+            using SqlCommand command = new SqlCommand();
+            command.CommandText = sql;
+            command.Connection = connection;
+            command.ExecuteNonQuery();
+
+
         }
     }
 }
